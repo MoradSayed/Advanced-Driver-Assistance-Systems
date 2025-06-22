2cd291f15c67636464efda9b3c1bb60856795794
Morad Sayed <Morad.S.Singer@gmail.com>
2025-06-22 19:18:09 +0300

    Upgrade LiDAR data processing
    - Add a callback parameter to the LiDAR class
    - Implement a new processing method based on a defined area
      instead of a fixed angle or FOV
    - Reorganize `adas.py` to support a cleaner base for ACC development

--- Grad_ws/src/vehicle_controller/vehicle_controller/adas.py
+++ Grad_ws/src/vehicle_controller/vehicle_controller/adas.py
@@ -1,6 +1,7 @@
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from .manager import Manager
+
 from std_msgs.msg import Float64
 
 from .sensors.Wheel_speed import WheelSpeed
@@ -10,27 +11,31 @@ from .closed_loop.pid_controller import PID
 
 class ADAS:
     def __init__(self, manager: "Manager"):
-        self.speed_pub = manager.speed_manager
-        self.brake_pub = manager.brake_manager
-        self.steer_pub = manager.steer_manager
+        self.manager = manager
+        self.is_acc = False
 
-        self.actual_speed = lambda: manager.vdriver.act_spd
-        
-        self.ws_sensor = WheelSpeed(manager, self.process, 0.374)   # (tire radius = 0.374) obtained from the BmwX5Wheel.proto. https://github.com/cyberbotics/webots/blob/9b5ed70644d66a2b405a039a521899b511102611/projects/vehicles/protos/bmw/BmwX5Wheel.proto
+        # self.ws_sensor = WheelSpeed(manager, self.process, 0.374)   # (tire radius = 0.374) obtained from the BmwX5Wheel.proto. https://github.com/cyberbotics/webots/blob/9b5ed70644d66a2b405a039a521899b511102611/projects/vehicles/protos/bmw/BmwX5Wheel.proto
         
         self.current_time = 0.0
-        manager.node.create_subscription(Float64, "/wbts_time", 
-                                         lambda t: setattr(self, "current_time", t.data), 
-                                         10)    #? to be replaced with real time in practical trials
+        manager.node.create_subscription(
+            Float64, "/wbts_time",
+            lambda t: setattr(self, "current_time", t.data),
+            10) #? to be replaced with real time in practical trials
 
-        Target_distance = 200
+        safe_distance = 15
         # self.graph = Plot(plot_point = Target_distance, timer_creator = manager.node.create_timer)
-        # self.accel_controller = PID(2, 0.0011, 0.0085, Target_distance, (0, 60))
-        
-        self.lidar = LidarSensor(manager)
+        self.accel_controller = PID(0.72, 0.0011, 0.0085, safe_distance, (0, 0.6))  #TODO: Tune the controller
+        self.lidar = LidarSensor(manager, callback=self.process)
 
-    def process(self, distance_traveled):
-        # pedal = self.accel_controller.compute(distance_traveled, self.current_time)
-        # self.speed_pub(float(pedal))
-        # self.graph.add_point(distance_traveled, self.current_time)
-        pass
+    def toggle_acc(self, state):
+        self.is_acc = state
+        print(f"ACC is {'on' if state else 'off'}")
+
+    def process(self, distance):
+        if self.is_acc:
+            if distance == float('inf'):
+                pedal = 0.6
+            else:
+                pedal = 0.6 - self.accel_controller.compute(distance, self.current_time)
+            self.manager.speed_manager(float(pedal))
+            # self.graph.add_point(distance_traveled, self.current_time)
--- Grad_ws/src/vehicle_controller/vehicle_controller/sensors/lidar.py
+++ Grad_ws/src/vehicle_controller/vehicle_controller/sensors/lidar.py
@@ -1,40 +1,70 @@
 from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from ..manager import Manager
-import numpy as np
+from typing import Callable
 
+import numpy as np
 from sensor_msgs.msg import LaserScan
 
 class LidarSensor():
     
-    def __init__(self, manager: "Manager", fov:int = 10):
+    def __init__(self, manager: "Manager", callback: Callable, fov:int = None):
         self.fov = fov
         self.manager = manager
+        self.callback = callback
 
         self.start_index = None
         self.end_index = None
 
-        self.x = self.init_cahce
-        manager.node.create_subscription(LaserScan, "/av_lidar", lambda msg: self.x(msg), 10)
-        
-    def init_cahce(self, scan_data: LaserScan):
+        #? Initial method to cache contant values
+        self._process_callback = self.cache_constants #? is replaced with `process_lidar` after the first call
+        manager.node.create_subscription(LaserScan, "/av_lidar", lambda msg: self._process_callback(msg), 10)
+
+    def cache_constants(self, scan_data: LaserScan):
         ranges = np.array(scan_data.ranges, dtype=np.float32)
+        if self.fov:
+            #? Method 1: Angle constraints
+            angle_increment = scan_data.angle_increment
+            middle_index = len(ranges) // 2
+            num_angles = int(self.fov * (np.pi/180) / angle_increment)
 
-        angle_increment = scan_data.angle_increment
-        middle_index = len(ranges) // 2
-        num_angles = int(self.fov * (np.pi/180) / angle_increment)  # 10 degrees 
+            self.start_index = max(0, middle_index - num_angles)
+            self.end_index = min(len(ranges) - 1, middle_index + num_angles)
 
-        self.start_index = max(0, middle_index - num_angles)
-        self.end_index = min(len(ranges) - 1, middle_index + num_angles)
+            self._process_callback = self.process_lidar_angle
+            self.process_lidar_angle(scan_data=scan_data)
+        else:
+            #? Method 2: Area constraints (Rectangle Shape)
+            self.rad_angles = scan_data.angle_min + np.arange(len(ranges)) * scan_data.angle_increment
+            self.half_width = 2.8 / 2
+            self.max_range = scan_data.range_max
 
-        self.x = self.process_lidar
-        self.process_lidar(scan_data=scan_data)
+            self._process_callback = self.process_lidar_rect
+            self.process_lidar_rect(scan_data=scan_data)
 
-    def process_lidar(self, scan_data: LaserScan):
+    def process_lidar_angle(self, scan_data: LaserScan):
         ranges = np.array(scan_data.ranges, dtype=np.float32)
         front_distances = ranges[self.start_index:self.end_index+1]
         valid_distances = front_distances[(front_distances > 0) & (front_distances < np.inf)]
         
         if valid_distances.size > 0:
             min_distance = np.min(valid_distances)
-            self.manager.node.get_logger().info(f"min_dist: {min_distance}")
+        else:
+            min_distance = float('inf')
+        print("\r\033[K"+f"min_dist: {min_distance:.2f}", end="")
+        self.callback(min_distance)
+
+    def process_lidar_rect(self, scan_data: LaserScan):   #! Make sure angles are in the correct order.
+        ranges = np.array(scan_data.ranges)
+        # print(self.rad_angles)
+
+        # Convert to Cartesian coordinates
+        x = ranges * np.cos(self.rad_angles)
+        y = ranges * np.sin(self.rad_angles)
+
+        valid = (np.abs(y) <= self.half_width) & (x >= 0) & (x <= self.max_range)
+        masked_ranges = np.where(valid, ranges, float('inf'))   # Apply mask: keep valid values, set others to inf
+
+        min_distance = np.min(masked_ranges)
+        print("\r\033[K"+f"min_dist: {min_distance:.2f}", end="")
+        self.callback(min_distance)
